9276c4cd86b13cb35062afe08f90f5ef263c588b
Ongyj888 <67620716+Ongyj888@users.noreply.github.com>
2022-10-23 11:14:34 +0800

    Add files via upload

---
+++ Main.bat
@@ -0,0 +1,29 @@
+@echo off
+title KRNL Bypasser v2 (renewed version)
+echo first
+echo https://cdn.krnl.place/getkey
+echo Do captcha and press any key here
+pause >nul
+timeout /t 015 /nobreak >nul
+echo second
+start https://cdn.krnl.place/getkey_scripts
+echo Do captcha and press any key here
+pause >nul
+timeout /t 015 /nobreak >nul
+echo third
+echo https://cdn.krnl.place/getkey.php
+echo Do captcha and press any key here
+pause >nul
+timeout /t 015 /nobreak >nul
+echo fourth
+start https://cdn.krnl.place/getkey_games
+echo Do captcha and press any key here
+pause >nul
+timeout /t 015 /nobreak >nul
+echo fifth
+echo https://cdn.krnl.place/interface
+echo Done
+echo Press any key to quit
+pause >nul
+exit /b
+
