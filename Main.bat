359193304bfc5e59f87cd39be178c10806db2faf
Ongyj888 <67620716+Ongyj888@users.noreply.github.com>
2022-10-27 10:32:08 +0800

    dumb big fixes (/interface fix + 16 sec timeout)

--- Main.bat
+++ Main.bat
@@ -4,24 +4,24 @@ echo first
 start https://cdn.krnl.place/getkey
 echo Do captcha and press any key here
 pause >nul
-timeout /t 015 /nobreak >nul
+timeout /t 016 /nobreak >nul
 echo second
 start https://cdn.krnl.place/getkey_scripts
 echo Do captcha and press any key here
 pause >nul
-timeout /t 015 /nobreak >nul
+timeout /t 016 /nobreak >nul
 echo third
 start https://cdn.krnl.place/getkey.php
 echo Do captcha and press any key here
 pause >nul
-timeout /t 015 /nobreak >nul
+timeout /t 016 /nobreak >nul
 echo fourth
 start https://cdn.krnl.place/getkey_games
 echo Do captcha and press any key here
 pause >nul
-timeout /t 015 /nobreak >nul
+timeout /t 016 /nobreak >nul
 echo fifth
-start https://cdn.krnl.place/interface
+start https://cdn.krnl.place_interface
 echo Done
 echo Press any key to quit
 pause >nul
